8d09a2d238449718f7c9eb06dd4c97857dc4eb9d
diamar905 <diamar.905@gmail.com>
2024-08-11 16:50:25 -0500

    Segunda clase actualizada

---
+++ Clase2/vite-project/src/assets/Componente.jsx
@@ -0,0 +1,3 @@
+export default function Componente(){
+    return <h1>Componente 1: Animes vieja escuela</h1>
+}
---
+++ Clase2/vite-project/src/assets/imagenes.js
@@ -0,0 +1,7 @@
+import goku from './img/goku.jpeg';
+import sailor from './img/sailor.jpg';
+
+export default {
+    "img1": goku,
+    "img2": sailor
+}
